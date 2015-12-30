3869ca94b1aeadce214b8216d9d6b2aaeca475af
Peter <monroepe@gmail.com>
2015-12-29 21:55:59 -0500

    Can add items and displays items

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('groceryList', [
   'ngRoute',
+  "firebase",
   'groceryList.groceryList'
 ]).
 config(['$routeProvider', function($routeProvider) {
--- app/groceryList/groceryList.js
+++ app/groceryList/groceryList.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('groceryList.groceryList', ['ngRoute'])
+angular.module('groceryList.groceryList', ['ngRoute', 'firebase'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/groceryList', {
@@ -9,6 +9,39 @@ angular.module('groceryList.groceryList', ['ngRoute'])
   });
 }])
 
-.controller('GroceryListCtrl', [function() {
+.controller('GroceryListCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+  var ref = new Firebase('https://thegrocerylist.firebaseio.com/groceries');
 
+  $scope.groceries = $firebaseArray(ref);
+
+  $scope.addFormSubmit = function(){
+    console.log('Adding Grocery...');
+
+    // Assign Values
+    if($scope.name){ var name = $scope.name } else { var name = null; }
+    if($scope.amount){ var amount = $scope.amount; } else { var amount = null; }
+    if($scope.description){ var description = $scope.description; } else { var description = null; }
+
+    // Build Object
+    $scope.groceries.$add({
+      name: name,
+      amount: amount,
+      description: description
+    }).then(function(ref){
+
+      // Clear Form
+      clearFields();
+
+      // Send Message
+      $scope.msg = "Item Added";
+    });
+  }
+
+  function clearFields(){
+    console.log('Clearing All Fields...');
+
+    $scope.name = '';
+    $scope.amount = '';
+    $scope.description = '';
+  }
 }]);
