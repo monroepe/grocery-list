b86e21b151b498b3ab33e0e26a7a319a6bff9f23
Peter <monroepe@gmail.com>
2016-02-26 20:36:12 -0500

    Cleaned up grocery list controller

--- app/groceryList/groceryList.js
+++ app/groceryList/groceryList.js
@@ -25,45 +25,30 @@ angular.module('groceryList.groceryList', ['ngRoute', 'firebase'])
   $scope.addFormShow = true;
 
   $scope.addFormSubmit = function(){
-    console.log('Adding Grocery...');
-
-    // Assign Values
     if($scope.name){ var name = $scope.name } else { var name = null; }
     if($scope.description){ var description = $scope.description; } else { var description = null; }
 
-    // Build Object
     $scope.groceries.$add({
       name: name,
       bought: false,
       userId: $scope.userId
     }).then(function(ref){
 
-      // Clear Form
-      clearFields();
-
-      // Send Message
-      $scope.msg = "Item Added";
+    clearFields();
+    $scope.msg = "Item Added";
     });
   }
 
   $scope.editFormSubmit = function(){
-    console.log('Updating Grocery...');
-
     var id = $scope.id;
     var record = $scope.groceries.$getRecord(id);
 
     record.name = $scope.name;
-
-    $scope.groceries.$save(record).then(function(ref){
-      console.log(ref.key);
-    });
+    $scope.groceries.$save(record);
 
     clearFields();
-
-    // Hide Form
     $scope.editFormShow = false;
     $scope.addFormShow = true;
-
     $scope.msg = "Grocery Updated";
   }
 
@@ -76,32 +61,21 @@ angular.module('groceryList.groceryList', ['ngRoute', 'firebase'])
   }
 
   function clearFields(){
-    console.log('Clearing All Fields...');
-
     $scope.name = '';
   }
 
   $scope.bought = function(id){
     var record = $scope.groceries.$getRecord(id);
-
     record.bought = (record.bought == true ? false : true);
-
-    $scope.groceries.$save(record).then(function(ref){
-      console.log(record.name + " bought!");
-    });
+    $scope.groceries.$save(record);
   }
 
   $scope.removeGrocery = function(grocery){
-    console.log('Removing Grocery');
-
     $scope.groceries.$remove(grocery);
-
     $scope.msg="Grocery Removed";
   }
 
   $scope.clearPurchased = function(){
-    console.log('Clearing purchased');
-
     $scope.groceries = $scope.groceries.filter(function(grocery) {
       if (grocery.bought) {
         $scope.groceries.$remove(grocery);
